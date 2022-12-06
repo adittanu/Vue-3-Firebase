5c9ba7a35b90c732047257964cccdaba244072c4
Adit Tanu <adiettanu27@gmail.com>
2022-12-06 12:42:40 +0700

    v-for

--- app.js
+++ app.js
@@ -2,26 +2,16 @@ const app = Vue.createApp({
     data() {
       return {
         showBooks: true,
-        title: 'The Way of Kings',
-        author: 'Brandon Sanderson',
-        age: 45,
-        x: 0,
-        y: 0
+        books : [
+            { title: 'name of the wind', author: 'patrick rothfuss'},
+            { title: 'the way of kings', author: 'Brandon sanderson'},
+            { title: 'The Final Empire', author: 'Brandon sanderson'},
+        ]
       }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
-        },
-        handleEvent(e, number) {
-            console.log(e);
-            if(number) {
-                console.log(number)
-            }
-        },
-        handleMouseMove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
         }
     },
   })
