a1f1d6fe6cf24f65999db6c31dc84fbff5c6f8e5
Adit Tanu <adiettanu27@gmail.com>
2022-12-06 13:27:15 +0700

    dynamic data

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
         url : 'http://www.thenetninja.co.uk',
         showBooks: true,
         books : [
-            { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
-            { title: 'the way of kings', author: 'Brandon sanderson', img: 'assets/2.jpg'},
-            { title: 'The Final Empire', author: 'Brandon sanderson', img: 'assets/3.jpg'},
+            { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+            { title: 'the way of kings', author: 'Brandon sanderson', img: 'assets/2.jpg', isFav: false},
+            { title: 'The Final Empire', author: 'Brandon sanderson', img: 'assets/3.jpg', isFav: true},
         ]
       }
     },
