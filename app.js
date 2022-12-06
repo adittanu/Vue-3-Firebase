71a01f9a2b4b252c8feb3cfdea4d91bca34c14d4
Adit Tanu <adiettanu27@gmail.com>
2022-12-06 12:32:18 +0700

    mouse event

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
         showBooks: true,
         title: 'The Way of Kings',
         author: 'Brandon Sanderson',
-        age: 45
+        age: 45,
+        x: 0,
+        y: 0
       }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, number) {
+            console.log(e);
+            if(number) {
+                console.log(number)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     },
   })
